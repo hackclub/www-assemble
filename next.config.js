be3fa7e7c4a5269dbb0f540d5ba2800cee64a0b8
Sam Poder <39828164+sampoder@users.noreply.github.com>
2022-05-30 17:32:27 +0800

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -3,35 +3,12 @@ const { redirect } = require('next/dist/server/api-utils')
 const withMDX = require('@next/mdx')({ extension: /\.mdx?$/ })
 module.exports = withMDX({
   pageExtensions: ['js', 'mdx'],
-  async rewrites() {
+  async redirects() {
     return [
       {
         source: '/register',
         destination: 'https://www-assemble-register.hackclub.dev/',
-      },
-      {
-        source: '/:slug*',
-        destination: 'https://www-assemble-register.hackclub.dev/:slug*',
-      },
-      {
-        source: '/register/:slug*',
-        destination: 'https://www-assemble-register.hackclub.dev/register/:slug*',
-      },
-    ]
-  },
-  async redirects() {
-    return [
-      {
-        permanent: true,
-        source: `/register/_next/:path*`,
-        destination: 'https://www-assemble-register.hackclub.dev/register/_next/:path*',
-      },
-      /* for images */
-      {
-        permanent: true,
-        source: `/register/images/:query*`,
-        destination: 'https://www-assemble-register.hackclub.dev/register/_next/image/:query*',
-      },
+      }
     ]
   },
 })
