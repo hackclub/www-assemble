2dc8f85971b3b8b6258df28e3f278cc58b61d098
Max Wofford <max@maxwofford.com>
2022-06-11 17:44:11 -0400

    Dino easter egg is default hidden

--- components/dino-easter-egg.js
+++ components/dino-easter-egg.js
@@ -4,24 +4,32 @@ const DinoEasterEgg = (props) => {
   // return null
   return (
     <>
-      <Box>
-      </Box>
+      <Box
+        sx={{
+          position: 'fixed',
+          bottom: 0,
+          right: 0,
+          height: '240px',
+          width: '240px',
+          zIndex: '200',
+          '&:hover > img': {
+            bottom: '-80px',
+          },
+        }}>
       <Image
         src="/dinosaur-waving.png"
         sx={{
-          position: 'fixed',
-          bottom: -120,
+          position: 'relative',
+          bottom: '-100%',
           height: '240px',
           right: -45,
           zIndex: '200',
           cursor: 'pointer',
           transition: 'ease-in-out 0.3s',
-          '&:hover': {
-            bottom: '-80px',
-          },
         }}
         onClick={() => window.open('mailto:assemble@hackclub.com', '_blank')}
       />
+      </Box>
     </>
   )
 }
