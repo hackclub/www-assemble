21c75c1670cfe36072c69065db857d990b8fa4d1
Sam Poder <39828164+sampoder@users.noreply.github.com>
2022-06-22 23:02:53 +0800

    Update features.js

--- components/features.js
+++ components/features.js
@@ -2,30 +2,38 @@ import { Container, Grid, Box, Heading, Text, Link } from 'theme-ui'
 import Icon from '@hackclub/icons'
 import ComicImage from './comic-image'
 
-export const Features = () => (
+export const Rundown2 = () => (
   <Box
     sx={{
-      py: 4,
+      py: 5,
       backgroundAttachment: 'fixed',
-      backgroundImage: 'url(patterns/dots_blue.png), linear-gradient(90deg, rgba(255, 255, 255, 1) 0%, rgba(255, 255, 255, 1) 100% )',
+      backgroundImage:
+        'linear-gradient(90deg, rgba(0,0,0, 0.2) 0%, rgba(0,0,0, 0.2) 100% ), url(patterns/dots_blue.png)',
     }}
   >
-    <Container variant="copy">
+    <Container variant="copy" sx={{ maxWidth: ['copy', null, 'copyUltra'] }}>
       <Link href="https://youtu.be/oolpPmuK2I8" target="_blank">
         <Text
           variant="title"
           sx={{
             display: 'block',
-            my: 2
+            my: 2,
+            color: 'white',
+            textShadow: 'elevated',
+            textDecoration: 'underline',
+            mb: 4,
           }}
         >
-          Come together...
+          Come together to...
         </Text>
       </Link>
       {/* <Text variant="title" color="white" sx={{display:['inline', null,'none']}}>Create a renaissance</Text> */}
       <Grid columns={[1, null, 2]}>
-        <InfoBox iconGlyph="support" heading="Share your knowledge">
-          At Assemble, you'll have the opportunity to give & watch talks from peers, gain experience, make connections, and discover yourself. You'll meet new people, get help from peers, and teach your fellow hackers.
+        <InfoBox iconGlyph="support" heading="Share Your Knowledge">
+          At Assemble, you'll have the opportunity to give & watch talks from
+          peers, gain experience, make connections, and discover yourself.
+          You'll meet new people, get help from peers, and teach your fellow
+          hackers.
         </InfoBox>
         <ComicImage
           // webp="https://cloud-nmdl7opdm-hack-club-bot.vercel.app/3hibernia.webp"
@@ -36,23 +44,23 @@ export const Features = () => (
           objectPosition="left bottom"
         />
 
-
-
         <ComicImage
           webp="https://cloud-nmdl7opdm-hack-club-bot.vercel.app/3hibernia.webp"
           src="https://cloud-me9ijbqml-hack-club-bot.vercel.app/0hibernia.png"
           href="https://cloud-kmmvp0jlq-hack-club-bot.vercel.app/0img_1372.jpg"
           alt="At the corner of Figma HQ"
         />
-        <InfoBox iconGlyph="explore" heading="Build for yourself">
-          At Assemble, projects are judged on "hacker value" by the people who matter most. Attendees.
+        <InfoBox iconGlyph="explore" heading="Build For Yourself">
+          At Assemble, projects are judged on "hacker value" by the people who
+          matter most. Attendees.
         </InfoBox>
 
-
-        <InfoBox iconGlyph="announcement" heading="Hack the planet">
-          One hackathon doesn’t constitute a renaissance, so we’re open-sourcing and documenting everything we work on for Assemble
-          to allow others to create incredible hackathons in their communities. At the end of Assemble, we’ll launch a comprehensive
-          guide for hackathon organizing so you can reboot the hackathon ecosystem at your high school.
+        <InfoBox iconGlyph="announcement" heading="Hack The Planet">
+          One hackathon doesn’t constitute a renaissance, so we’re open-sourcing
+          and documenting everything we work on for Assemble to allow others to
+          create incredible hackathons in their communities. At the end of
+          Assemble, we’ll launch a comprehensive guide for hackathon organizing
+          so you can reboot the hackathon ecosystem at your high school.
         </InfoBox>
         <ComicImage
           webp="https://cloud-o7avz9t60-hack-club-bot.vercel.app/020191026_151117.webp"
@@ -69,21 +77,20 @@ const InfoBox = ({ iconGlyph, heading, children }) => (
   <Box
     sx={{
       p: 1,
+      pb: 4,
       bg: 'sunken',
       color: 'black',
       textAlign: 'left',
       backgroundSize: '30px',
-      backgroundImage:
-        'url(/patterns/white.png)',
-      border: '3px solid #FF544A',
+      backgroundImage: 'url(/patterns/white.png)',
+      border: '3px solid black'
     }}
   >
     <Box
       sx={{
         p: 3,
         display: 'flex',
-        gap: '8px',
-
+        gap: '12px',
       }}
     >
       <Box>
@@ -94,14 +101,14 @@ const InfoBox = ({ iconGlyph, heading, children }) => (
           sx={{
             display: 'flex',
             alignItems: 'center',
-            gap: '4px',
+            gap: '8px',
             ml: '-2px',
             width: 'fit-content',
-            fontSize: 32
+            fontSize: 32,
           }}
         >
-          <Box sx={{ color: '#FF544A', pt: '1px' }}>
-            <Icon glyph={iconGlyph} size={32} />
+          <Box sx={{ color: '#FF544A', display: 'flex', alignItems: 'center' }}>
+            <Icon glyph={iconGlyph} size={36} />
           </Box>
           {heading}
         </Heading>
