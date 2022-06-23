3d4ef94eb8c17d5156be20d55dd64d728e0e3d5a
Sam Poder <39828164+sampoder@users.noreply.github.com>
2022-06-23 23:04:51 +0800

    Fix up Meta!

--- components/meta.js
+++ components/meta.js
@@ -32,63 +32,60 @@ const Meta = ({
 
   return (
     <Head>
-      <meta key="og_locale" property="og:locale" content="en_US" />
-      <meta key="og_type" property="og:type" content="website" />
-      <meta key="og_site" property="og:site_name" content={name} />
-      <title key="title">{focus ? 'Come together...' : '...over me'}</title>
-      <meta key="og_title" property="og:title" content={makeTitle(title, name)} />
-      <meta
-        key="tw_title"
-        name="twitter:title"
-        content={makeTitle(title, name)}
-      />
-      {description && (
-        <>
-          <meta key="desc" name="description" content={description} />
-          <meta key="og_desc" property="og:description" content={description} />
-          <meta key="tw_desc" name="twitter:description" content={description} />
-        </>
-      )}
-      {image && (
-        <>
-          <meta key="og_img" property="og:image" content={image} />
-          <meta key="tw_card" name="twitter:card" content="summary_large_image" />
-          <meta key="tw_img" name="twitter:image" content={image} />
-        </>
-      )}
-      <meta key="theme_color" name="theme-color" content={theme.colors.primary} />
-      <meta
-        key="tile_color"
-        name="msapplication-TileColor"
-        content={theme.colors.primary}
-      />
-      <link
-        key="safari_icon"
-        rel="mask-icon"
-        href={`${url}/safari-pinned-tab.png`}
-        color={theme.colors.primary}
-      />
-      <link
-        key="apple_icon"
-        rel="apple-touch-icon"
-        sizes="180x180"
-        href={`${url}/apple-touch-icon.png`}
-      />
-      <link
-        key="favicon_32"
-        rel="icon"
-        type="image/png"
-        sizes="32x32"
-        href={`${url}/favicon-32x32.png`}
-      />
-      <link
-        key="favicon_16"
-        rel="icon"
-        type="image/png"
-        sizes="16x16"
-        href={`${url}/favicon-16x16.png`}
-      />
-      {children}
+     <meta key="og_locale" property="og:locale" content="en_US" />
+    <meta key="og_type" property="og:type" content="website" />
+    <meta key="og_site" property="og:site_name" content={name} />
+    <meta key="tw_site" name="twitter:site" content="@hackclub" />
+    <title key="title">{makeTitle(title, name)}</title>
+    <meta key="og_title" property="og:title" content={makeTitle(title, name)} />
+    <meta
+      key="tw_title"
+      name="twitter:title"
+      content={makeTitle(title, name)}
+    />
+    {description && (
+      <React.Fragment>
+        <meta key="desc" name="description" content={description} />
+        <meta key="og_desc" property="og:description" content={description} />
+        <meta key="tw_desc" name="twitter:description" content={description} />
+      </React.Fragment>
+    )}
+    {image && (
+      <React.Fragment>
+        <meta key="og_img" property="og:image" content={image} />
+        <meta key="tw_card" name="twitter:card" content="summary_large_image" />
+        <meta key="tw_img" name="twitter:image" content={image} />
+      </React.Fragment>
+    )}
+    <meta key="theme_color" name="theme-color" content={theme.colors.primary} />
+    <meta key="tile_color" name="msapplication-TileColor" content={theme.colors.primary} />
+    <link
+      key="safari_icon"
+      rel="mask-icon"
+      href="https://assets.hackclub.com/favicons/safari-pinned-tab.svg"
+      color={theme.colors.primary}
+    />
+    <link
+      key="apple_icon"
+      rel="apple-touch-icon"
+      sizes="180x180"
+      href="https://assets.hackclub.com/favicons/apple-touch-icon.png"
+    />
+    <link
+      key="favicon_32"
+      rel="icon"
+      type="image/png"
+      sizes="32x32"
+      href="https://assets.hackclub.com/favicons/favicon-32x32.png"
+    />
+    <link
+      key="favicon_16"
+      rel="icon"
+      type="image/png"
+      sizes="16x16"
+      href="https://assets.hackclub.com/favicons/favicon-16x16.png"
+    />
+    {children}
     </Head>
   )
 }
