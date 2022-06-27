7b3ce86c260d6aa87071b22dede75efb2daa16bb
citricacid2 <61765149+citricacid2@users.noreply.github.com>
2022-06-27 10:59:22 -0400

    added konami code easter egg

--- pages/index.js
+++ pages/index.js
@@ -13,7 +13,7 @@ import { useEffect, useState } from 'react'
 import Registration, { cache, registrants } from '../components/registration'
 
 export default function Index() {
-  const [showRegistration, setShowRegistration] = useState(false);
+  const [showRegistration, setShowRegistration] = useState(false)
 
   useEffect(() => {
     console.log(` █████╗ ███████╗███████╗███████╗███╗   ███╗██████╗ ██╗     ███████╗
@@ -23,24 +23,64 @@ export default function Index() {
 ██║  ██║███████║███████║███████╗██║ ╚═╝ ██║██████╔╝███████╗███████╗
 ╚═╝  ╚═╝╚══════╝╚══════╝╚══════╝╚═╝     ╚═╝╚═════╝ ╚══════╝╚══════╝
 
-Hey hacker! Register for Assemble at https://assemble.hackclub.com/register. We hope to see you there!`);
+Hey hacker! Register for Assemble at https://assemble.hackclub.com/register. We hope to see you there!`)
     if (!window.enableValidation) {
       setTimeout(() => {
-        cache(registrants);
-      }, 8000);
-      let input = ``;
-      window.enableValidation = true;
+        cache(registrants)
+      }, 8000)
+      let input = ``
+      window.enableValidation = true
       window.onkeydown = async event => {
-        input += event.key.toLowerCase();
-        if (!input.endsWith('e')) return;
+        input += event.key.toLowerCase()
+        if (!input.endsWith('e')) return
 
         const { success } = await fetch('/api/validation', {
-          method: 'POST', headers: { 'Content-Type': 'application/json' }, body: JSON.stringify({ input })
-        }).then(res => res.json());
+          method: 'POST',
+          headers: { 'Content-Type': 'application/json' },
+          body: JSON.stringify({ input }),
+        }).then(res => res.json())
 
-        if (success) setShowRegistration(true);
+        if (success) setShowRegistration(true)
       }
     }
+    // Credit to https://workshops.hackclub.com/konami_code/ for the logic of the konami code.
+    const keys = []
+    const secretCode = [
+      'ArrowUp',
+      'ArrowUp',
+      'ArrowDown',
+      'ArrowDown',
+      'ArrowLeft',
+      'ArrowRight',
+      'ArrowLeft',
+      'ArrowRight',
+      'b',
+      'a',
+      'Enter',
+    ]
+    window.addEventListener('keyup', e => {
+      keys.push(e.key)
+      if (keys.length > secretCode.length) {
+        keys.shift()
+      }
+      if (JSON.stringify(keys) === JSON.stringify(secretCode)) {
+        // Credit to https://github.com/sc420/jellify-ur-website for designing this amazing jellifing bookmarklet!
+
+        const urls = [
+          // Dependencies should be loaded first
+          'https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js',
+          'https://cdn.jsdelivr.net/npm/matter-js@0.17.1/build/matter.min.js',
+          // Main script is loaded last
+          'https://sc420.github.io/jellify-ur-website/js/jellify.js',
+        ]
+        urls.forEach(url => {
+          const head = document.getElementsByTagName('head')[0]
+          const script = document.createElement('script')
+          script.src = url
+          head.appendChild(script)
+        })
+      }
+    })
   }, [])
   return (
     <>
