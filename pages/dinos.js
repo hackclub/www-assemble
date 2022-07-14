8d617fc1c920c0bd72d57838abd7e5e4fea4820e
YodaLightsabr <76178582+YodaLightsabr@users.noreply.github.com>
2022-07-14 16:31:57 -0700

    add caching to dino page

--- pages/dinos.js
+++ pages/dinos.js
@@ -1,8 +1,18 @@
 import Registration from '../components/registration'
 import AudioCheck from '../components/audio-check'
+import { useEffect } from 'react'
+import Registration, { cache, registrants } from '../components/registration'
 
-export default () => (
-  <AudioCheck>
-    <Registration />
-  </AudioCheck>
-)
+export default () => {
+  useEffect(() => {
+    setTimeout(() => {
+      cache(registrants);
+    }, 1000);
+  }, []);
+
+  return (
+    <AudioCheck>
+      <Registration />
+    </AudioCheck>
+  );
+}
